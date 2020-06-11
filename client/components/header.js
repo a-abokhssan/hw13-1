ff5b58926ae48493c964524029f9a646b77126c1
Amir Abokhssan <a.abokhssan@gmail.com>
2020-06-11 14:21:27 +0300

    added sum of products

--- client/components/header.js
+++ client/components/header.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
-  return <div>123</div>
+  const list = useSelector((s) => s.products.list)
+  const selection = useSelector((s) => s.products.selection)
+  const getPrice = (id) => list.find((it) => it.id === id).price
+  const sum = Object.entries(selection).reduce((acc, [id, qty]) => acc + getPrice(id) * qty, 0)
+  const numberOfItems = Object.values(selection).reduce((acc, rec) => acc + rec, 0)
+  return (
+    <div>
+      <div>{sum !== 0 && sum}</div>
+      <div>{numberOfItems !== 0 && numberOfItems}</div>
+    </div>
+  )
 }
 
 export default Header
